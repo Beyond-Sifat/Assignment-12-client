9c02c5e3b33baa873ff10f156988fba1f7337cd1
student-webdev-ph <mdaltafuddin456@gmail.com>
2025-07-08 00:07:03 +0600

    Files added

---
+++ src/router/router.jsx
@@ -0,0 +1,19 @@
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router";
+import MainLayout from "../Layouts/MainLayout";
+import Home from "../Pages/Home/Home";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        Component: MainLayout,
+        children: [
+            {
+                index: true,
+                Component: Home
+            },
+        ]
+    },
+]);
